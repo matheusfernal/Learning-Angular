a9abb5ea0c3911f51b37464b14150b32c6e1bfd7
Matheus Fernal <matheusfernal@gmail.com>
2017-10-03 23:51:36 -0300

    Missing semicolon

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -13,6 +13,6 @@ export class HeroService {
     return new Promise(resolve => {
       // Simulate server latency with 2 second delay
       setTimeout(() => resolve(this.getHeroes()), 2000);
-    })
+    });
   }
 }
