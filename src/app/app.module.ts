4ca203035c7390c2414d543e7d0acc62b77cb5ff
Matheus Fernal <matheusfernal@gmail.com>
2017-10-13 21:21:24 -0300

    Adding a search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { DashboardComponent } from './dashboard.component';
+import { HeroSearchComponent } from './hero-search.component';
 import { HeroService } from './hero.service';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -20,8 +21,9 @@ import { InMemoryDataService } from './in-memory-data.service';
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    DashboardComponent
-  ],
+    DashboardComponent,
+    HeroSearchComponent
+],
   imports: [
     BrowserModule,
     FormsModule,
